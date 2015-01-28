19c3c50a6990f58e8861b72d7a440fed6f108330
Gareth Lewin <gareth@justin.tv>
2015-01-28 14:00:54 -0800

    Changes to replace molecule for building
    - Added build.sh and build/packer.json
    - Removed old build scripts
    - Updated prints in metadata/postgres.go to printfs
    - Updated scripts/*.sh to assume the binary will be in the same folder
      as the script

--- build/scripts/run_dbstorer.sh
+++ build/scripts/run_dbstorer.sh
@@ -8,6 +8,6 @@ eval "$(curl 169.254.169.254/latest/user-data/)"
 export HOST="$(curl 169.254.169.254/latest/meta-data/hostname)"
 export STATSD_HOSTPORT="localhost:8125"
 
-exec ../dbstorer \
+exec ./dbstorer \
   -statsPrefix="${CLOUD_APP}-dbstorer.${CLOUD_DEV_PHASE:-${CLOUD_ENVIRONMENT}}.${EC2_REGION}.${CLOUD_AUTO_SCALE_GROUP##*-}" \
   -databaseURL="${INGESTER_DB_PROD}"
