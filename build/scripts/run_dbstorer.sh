539cb2bdb8addcce869102f6aec211abe06b6d06
Matt Aasted <aasted@twitch.tv>
2015-03-17 17:56:36 -0700

    Move config to s3

--- build/scripts/run_dbstorer.sh
+++ build/scripts/run_dbstorer.sh
@@ -2,12 +2,13 @@
 set -e -u -o pipefail
 
 cd -- "$(dirname -- "$0")"
-
 eval "$(curl 169.254.169.254/latest/user-data/)"
-
 export HOST="$(curl 169.254.169.254/latest/meta-data/hostname)"
 export STATSD_HOSTPORT="localhost:8125"
+export CONFIG_PREFIX="s3://$S3_CONFIG_BUCKET/$VPC_SUBNET_TAG/$CLOUD_APP/$CLOUD_ENVIRONMENT"
+aws s3 cp --region us-west-2 "$CONFIG_PREFIX/conf.sh" conf.sh
+source conf.sh
 
 exec ./dbstorer \
   -statsPrefix="${CLOUD_APP}-dbstorer.${CLOUD_DEV_PHASE:-${CLOUD_ENVIRONMENT}}.${EC2_REGION}.${CLOUD_AUTO_SCALE_GROUP##*-}" \
-  -databaseURL="${INGESTER_DB_PROD}"
+  -databaseURL="${INGESTER_DB_URL}"
--- build/scripts/run_ingester.sh
+++ build/scripts/run_ingester.sh
@@ -2,15 +2,16 @@
 set -e -u -o pipefail
 
 cd -- "$(dirname -- "$0")"
-
 eval "$(curl 169.254.169.254/latest/user-data/)"
-
 export HOST="$(curl 169.254.169.254/latest/meta-data/hostname)"
 export STATSD_HOSTPORT="localhost:8125"
+export CONFIG_PREFIX="s3://$S3_CONFIG_BUCKET/$VPC_SUBNET_TAG/$CLOUD_APP/$CLOUD_ENVIRONMENT"
+aws s3 cp --region us-west-2 "$CONFIG_PREFIX/conf.sh" conf.sh
+source conf.sh
 
 exec ./rs_ingester \
   -n_workers 5 \
   -statsPrefix="${CLOUD_APP}.${CLOUD_DEV_PHASE:-${CLOUD_ENVIRONMENT}}.${EC2_REGION}.${CLOUD_AUTO_SCALE_GROUP##*-}" \
   -scoopURL="${SCOOP_URL}" \
-  -databaseURL="${INGESTER_DB_PROD}" \
+  -databaseURL="${INGESTER_DB_URL}" \
   -manifestBucket="rsingester-manifests"
