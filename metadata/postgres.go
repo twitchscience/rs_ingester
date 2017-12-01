049041fd7c09bfaf1c079cbddcc5ae200bf5f117
Jack Gao <jackgao@twitch.tv>
2017-12-01 11:53:50 -0800

    debug

--- metadata/postgres.go
+++ metadata/postgres.go
@@ -372,7 +372,7 @@ func (b *postgresBackend) loadDoneHelper(tx *sql.Tx, manifestUUID string, tableN
 	doneTime := time.Now().In(time.UTC)
 	logger.Info(tableName)
 	_, err = tx.Query(`
-		SELECT '$1'::varchar AS tablename, '$2'::timestamp AS last_loaded
+		SELECT $1::varchar AS tablename, $2::timestamp AS last_loaded
 		INTO TEMP TABLE ll_stage
 		`, tableName, doneTime)
 	if err != nil {
